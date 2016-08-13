5f489e37971072dc6bba1c9306b116e64c77799f
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-08-13 17:47:12 +0100

    start package with dummy code for now, and start notebook

---
+++ text_mining_toolkit/corpus_reader.py
@@ -0,0 +1,14 @@
+# module to read corpora
+# a corpus will be a directory with a plain text files
+
+def get_documents():
+    print("get documents")
+    pass
+
+def get_all_text():
+    print("get all text")
+    pass
+
+def get_text_by_document():
+    print("get text by dooucment")
+    pass
