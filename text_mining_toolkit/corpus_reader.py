8297c423ab091a3462fc9989355b80eb15d45baa
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-08-14 15:13:11 +0100

    fix package modules

--- text_mining_toolkit/corpus_reader.py
+++ text_mining_toolkit/corpus_reader.py
@@ -1,14 +1,27 @@
 # module to read corpora
 # a corpus will be a directory with a plain text files
 
-def get_documents():
-    print("get documents")
-    pass
+# glob module for finding files that match a pattern
+import glob
 
-def get_all_text():
-    print("get all text")
-    pass
+class CorpusReader:
 
-def get_text_by_document():
-    print("get text by dooucment")
-    pass
+    def __init__(self, directory_of_files, text_filename_pattern):
+        self.directory_of_files = directory_of_files
+        self.text_filename_pattern = text_filename_pattern
+
+        print("directory of files = ", self.directory_of_files)
+        print("text_filename_pattern = ", self.text_filename_pattern)
+        pass
+
+    def get_documents(self):
+        print("get documents")
+        pass
+
+    def get_all_text(self):
+        print("get all text")
+        pass
+
+    def get_text_by_document(self):
+        print("get text by dooucment")
+        pass
