ab9863f3ce0994991f69ffc41b81e968635afc0e
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-08-18 00:16:03 +0100

    improve whitespace removal

--- text_mining_toolkit/pre_processor.py
+++ text_mining_toolkit/pre_processor.py
@@ -5,14 +5,28 @@ import string
 
 
 # make lowercase
-def make_lowercase(nput_text):
+def to_lowercase(input_text):
     output_text = input_text.lower()
+    return output_text
+
+
+# remove whitespace, convert to single space
+def simplify_whitespace(input_text):
+    #output_text = input_text.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace)))
+    output_text = " ".join(input_text.split())
     return output_text
 
 
 # remove punctuation
 def remove_punctuation(input_text):
     output_text = input_text.translate(str.maketrans({a:None for a in string.punctuation}))
+    return output_text
+
+
+# keep only alphanumeric characters
+def keep_alphanumeric(input_text):
+    delchars = ''.join(c for c in map(chr, range(256)) if c not in (" " + string.ascii_letters + string.digits))
+    output_text = input_text.translate(str.maketrans("\n\r", "  ", delchars))
     return output_text
 
 
