5dacd0c9f8c3840f09fa1c6c114fe3a262ccedb8
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-08-23 23:51:24 +0100

    improve indexing

--- text_mining_toolkit/index_search.py
+++ text_mining_toolkit/index_search.py
@@ -17,6 +17,18 @@ def clear_index(content_directory):
     pass
 
 
+# print existing index
+def print_index(content_directory):
+    index_file = content_directory + "index"
+    if os.path.isfile(index_file):
+        with open(index_file, "rb") as f:
+            index = pickle.load(f)
+            pass
+        for x in index.items():
+            print(x)
+    pass
+
+
 # update index
 def update_index(content_directory, document_name, doc_words_list):
     # start with empty index
@@ -32,10 +44,15 @@ def update_index(content_directory, document_name, doc_words_list):
     pass
 
     # update index
-
+    # first make (word, document_name) tuple
+    doc_and_word = [(word, document_name) for word in doc_words_list]
+    # then count it
+    doc_word_count = collections.Counter(doc_and_word)
+    index += doc_word_count
 
     # finally save updated index again
     with open(index_file, "wb") as f:
+        print("saving updated index, ", index_file)
         pickle.dump(index, f)
         pass
 
