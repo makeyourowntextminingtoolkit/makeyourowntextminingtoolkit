fc569ab89a0c139f5fcffd6a2177ee8a81588949
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-09-03 18:19:08 +0100

    more work on indexing with pandas frames

--- text_mining_toolkit/index_search.py
+++ text_mining_toolkit/index_search.py
@@ -22,8 +22,10 @@ def clear_index(content_directory):
 # print existing index
 def print_index(content_directory):
     index_file = content_directory + "index"
-    index = pandas.read_pickle(f)
-    print(index)
+    index = pandas.read_pickle(index_file)
+    # max columns when printing .. (may not be needed if auto detected from display)
+    pandas.set_option('max_columns', 5)
+    print(index.head(10))
     pass
 
 
@@ -34,20 +36,22 @@ def update_index(content_directory, document_name, doc_words_list):
 
     # load index if it already exists
     index_file = content_directory + "index"
-    index = pandas.read_pickle(index_file)
-    pass
+    if os.path.isfile(index_file):
+        index = pandas.read_pickle(index_file)
+        pass
 
     # update index
     # (word, [document_name]) dictionary, there can be many [document_names] in list
     words_ctr = collections.Counter(doc_words_list)
     for w, c in words_ctr.items():
-        print("=== ", w, c)
-        index.ix[w, doc_name] = c
-    pass
+        #print("=== ", w, c)
+        index.ix[w, document_name] = c
+        pass
+    # replace NaN wirh zeros
+    index.fillna(0, inplace=True)
 
     # finally save updated index again
     index.to_pickle(index_file)
-
     pass
 
 
@@ -58,10 +62,12 @@ def search_index(content_directory, search_query):
     index_file = content_directory + "index"
     index = pandas.read_pickle(index_file)
 
-        # do query
-        matching_documents = index[search_query]
-        # count occurences in matching documents
-        matching_documents_counter = collections.Counter(matching_documents)
-        # return list of matching documents ordered by those with most occurences
-        return list(matching_documents_counter.most_common())
-    pass
+    # do query
+    documents = index.loc[search_query]
+    print("documents =", documents)
+    # filter out those with word count of zero
+    matching_documents = documents[documents > 0]
+    print("matching_documents", matching_documents)
+    # to list
+    matching_documents_list = [(k,v) for (k,v) in matching_documents.to_dict().items()]
+    return matching_documents_list
