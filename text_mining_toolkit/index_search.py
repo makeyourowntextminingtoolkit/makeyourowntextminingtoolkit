beeea914338694444a618b0ee9ea4da9d549e37e
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-09-04 00:08:14 +0100

    separate wordcount and relevance indices

--- text_mining_toolkit/index_search.py
+++ text_mining_toolkit/index_search.py
@@ -6,38 +6,51 @@ import os
 import collections
 # import pandas for index dataframe
 import pandas
-# import pickle for saving / restoring index
-import pickle
+# max columns when printing .. (may not be needed if auto detected from display)
+pandas.set_option('max_columns', 5)
 
 
-# clear (delete) index
-def clear_index(content_directory):
-    index_file = content_directory + "index"
-    if os.path.isfile(index_file):
-        os.remove(index_file)
-        print("removed index file: ", index_file)
+# delete indices
+def delete_indices(content_directory):
+    # delete wordcount index
+    wordcount_index_file = content_directory + "index.wordcount"
+    if os.path.isfile(wordcount_index_file):
+        os.remove(wordcount_index_file)
+        print("removed wordcount index file: ", wordcount_index_file)
+        pass
+
+    # delete relevance index
+    relevance_index_file = content_directory + "index.relevance"
+    if os.path.isfile(relevance_index_file):
+        os.remove(relevance_index_file)
+        print("removed relevance index file: ", relevance_index_file)
+        pass
     pass
 
 
 # print existing index
 def print_index(content_directory):
-    index_file = content_directory + "index"
-    index = pandas.read_pickle(index_file)
-    # max columns when printing .. (may not be needed if auto detected from display)
-    pandas.set_option('max_columns', 5)
-    print(index.head(10))
+    # wordcount index
+    wordcount_index_file = content_directory + "index.wordcount"
+    wordcount_index = pandas.read_pickle(wordcount_index_file)
+    print(wordcount_index.head(10))
+
+    # relevance index
+    relevance_index_file = content_directory + "index.relevance"
+    relevance_index_ = pandas.read_pickle(relevance_index_file)
+    print(relevance_index_.head(10))
     pass
 
 
-# update index
-def update_index(content_directory, document_name, doc_words_list):
+# update word count index
+def update_wordcount_index(content_directory, document_name, doc_words_list):
     # start with empty index
-    index = pandas.DataFrame()
+    wordcount_index = pandas.DataFrame()
 
     # load index if it already exists
-    index_file = content_directory + "index"
-    if os.path.isfile(index_file):
-        index = pandas.read_pickle(index_file)
+    wordcount_index_file = content_directory + "index.wordcount"
+    if os.path.isfile(wordcount_index_file):
+        wordcount_index = pandas.read_pickle(wordcount_index_file)
         pass
 
     # update index
@@ -45,13 +58,29 @@ def update_index(content_directory, document_name, doc_words_list):
     words_ctr = collections.Counter(doc_words_list)
     for w, c in words_ctr.items():
         #print("=== ", w, c)
-        index.ix[w, document_name] = c
+        wordcount_index.ix[w, document_name] = c
         pass
+
     # replace NaN wirh zeros
-    index.fillna(0, inplace=True)
+    wordcount_index.fillna(0, inplace=True)
 
     # finally save updated index again
-    index.to_pickle(index_file)
+    wordcount_index.to_pickle(wordcount_index_file)
+    pass
+
+
+# calculate relevance index from wordcount index
+def calculate_relevance_index(content_directory):
+    # load wordcount index
+    wordcount_index_file = content_directory + "index.wordcount"
+    wordcount_index = pandas.read_pickle(wordcount_index_file)
+
+    # word frequency (per document) from wordcount, aka TF
+    frequency_index = wordcount_index/wordcount_index.sum()
+
+    # save relevance index
+    relevance_index_file = content_directory + "index.relevance"
+    frequency_index.to_pickle(relevance_index_file)
     pass
 
 
@@ -59,12 +88,11 @@ def update_index(content_directory, document_name, doc_words_list):
 def search_index(content_directory, search_query):
     print("search_index called")
     # load index if it already exists
-    index_file = content_directory + "index"
-    index = pandas.read_pickle(index_file)
+    relevance_index_file = content_directory + "index.relevance"
+    relevance_index = pandas.read_pickle(relevance_index_file)
 
     # do query
-    documents = index.loc[search_query]
-    print("documents =", documents)
+    documents = relevance_index.loc[search_query]
     # filter out those with word count of zero
     matching_documents = documents[documents > 0]
     print("matching_documents", matching_documents)
