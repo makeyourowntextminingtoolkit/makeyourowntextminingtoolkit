e9bdc35e9ae65edec14004a8595e9ece06e98195
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2016-10-27 17:54:49 +0100

    refactor modules to clean up indexing types (wordcount, relevance, co-occurrence)

---
+++ text_mining_toolkit/index_relevance.py
@@ -0,0 +1,115 @@
+# module for indexing a corpus for relevance
+
+# glob module for finding files that match a pattern
+import glob
+# import os file deletion
+import os
+# import collections for index
+import collections
+# import pandas for index dataframe
+import pandas
+# import numpy for maths functions
+import numpy
+# max columns when printing .. (may not be needed if auto detected from display)
+pandas.set_option('max_columns', 5)
+
+
+# delete indices
+def delete_indices(content_directory):
+    # delete relevance index
+    relevance_index_file = content_directory + "index_relevance.hdf5"
+    if os.path.isfile(relevance_index_file):
+        os.remove(relevance_index_file)
+        print("removed relevance index file: ", relevance_index_file)
+        pass
+    pass
+
+
+# print existing index
+def print_index(content_directory):
+    # relevance index
+    relevance_index_file = content_directory + "index_relevance.hdf5"
+    hd5_store = pandas.HDFStore(relevance_index_file, mode='r')
+    relevance_index = hd5_store['corpus_index']
+    hd5_store.close()
+    print("relevance_index_file ", relevance_index_file)
+    print(relevance_index.head(10))
+    pass
+
+
+# calculate relevance index from wordcount index
+def calculate_relevance_index(content_directory):
+    # load wordcount index
+    wordcount_index_file = content_directory + "index_wordcount.hdf5"
+    hd5_store = pandas.HDFStore(wordcount_index_file, mode='r')
+    wordcount_index = hd5_store['corpus_index']
+    hd5_store.close()
+
+    # following is a workaround for a pandas bug
+    wordcount_index.index = wordcount_index.index.astype(str)
+
+    # word frequency (per document) from wordcount, aka TF
+    frequency_index = wordcount_index/wordcount_index.sum()
+
+    # penalise short word length
+    for word in frequency_index.index.values:
+        frequency_index.loc[word] = frequency_index.loc[word] * numpy.tanh(len(word)/5.0)
+        pass
+
+    # inverse document frequency
+    for word in frequency_index.index.values:
+        documents = frequency_index.loc[word]
+        matching_documents = documents[documents > 0]
+        inverse_document_frequency = 1.0 - (len(matching_documents) / len(documents))
+        # print("word =", word, " idf = ", inverse_document_frequency)
+        frequency_index.loc[word] = frequency_index.loc[word] * inverse_document_frequency
+        pass
+
+    # save relevance index
+    relevance_index_file = content_directory + "index_relevance.hdf5"
+    print("saving corpus relevance index ... ", relevance_index_file)
+    hd5_store = pandas.HDFStore(relevance_index_file, mode='w')
+    hd5_store['corpus_index'] = frequency_index
+    hd5_store.close()
+    pass
+
+
+# query relevance index
+def search_relevance_index(content_directory, search_query):
+    # load index if it already exists
+    relevance_index_file = content_directory + "index_relevance.hdf5"
+    hd5_store = pandas.HDFStore(relevance_index_file, mode='r')
+    relevance_index = hd5_store['corpus_index']
+    hd5_store.close()
+
+    # following is a workaround for a pandas bug
+    relevance_index.index = relevance_index.index.astype(str)
+
+    # query string to list of search terms
+    search_query_list = search_query.split()
+
+    # do query
+    documents = relevance_index.loc[search_query_list]
+    # sum the scores
+    results = documents.sum()
+    # multiply the scores
+    #results = documents.prod()
+    # filter out those with score of zero
+    results = results[results > 0]
+    return results.sort_values(ascending=False)
+
+
+# get words ordered by relevance (across all documents)
+def get_words_by_relevance(content_directory):
+    # load relevance index
+    relevance_index_file = content_directory + "index_relevance.hdf5"
+    hd5_store = pandas.HDFStore(relevance_index_file, mode='r')
+    relevance_index = hd5_store['corpus_index']
+    hd5_store.close()
+
+    # following is a workaround for a pandas bug
+    relevance_index.index = relevance_index.index.astype(str)
+
+    # sum the relevance scores for a word across all documents, sort
+    word_relevance = relevance_index.sum(axis=1).sort_values(ascending=False)
+    return word_relevance
--- text_mining_toolkit/index_wordcount.py
+++ text_mining_toolkit/index_wordcount.py
@@ -1,4 +1,4 @@
-# module for indexing a corpus, and basic search
+# module for indexing a corpus for wordcount
 
 # glob module for finding files that match a pattern
 import glob
@@ -10,24 +10,17 @@ import collections
 import pandas
 # import numpy for maths functions
 import numpy
-import numpy.lib.recfunctions
 # max columns when printing .. (may not be needed if auto detected from display)
 pandas.set_option('max_columns', 5)
 
 
 # delete indices
 def delete_indices(content_directory):
+    # delete wordcound index
     wordcount_index_file = content_directory + "index_wordcount.hdf5"
     if os.path.isfile(wordcount_index_file):
         os.remove(wordcount_index_file)
         print("removed wordcount index file: ", wordcount_index_file)
-        pass
-
-    # delete relevance index
-    relevance_index_file = content_directory + "index_relevance.hdf5"
-    if os.path.isfile(relevance_index_file):
-        os.remove(relevance_index_file)
-        print("removed relevance index file: ", relevance_index_file)
         pass
     pass
 
@@ -40,14 +33,6 @@ def print_index(content_directory):
     hd5_store1.close()
     print("wordcount_index_file ", wordcount_index_file)
     print(wordcount_index.head(10))
-
-    # relevance index
-    relevance_index_file = content_directory + "index_relevance.hdf5"
-    hd5_store2 = pandas.HDFStore(relevance_index_file, mode='r')
-    relevance_index = hd5_store2['corpus_index']
-    hd5_store2.close()
-    print("relevance_index_file ", relevance_index_file)
-    print(relevance_index.head(10))
     pass
 
 
@@ -110,43 +95,6 @@ def merge_wordcount_indices_for_corpus(content_directory):
     pass
 
 
-# calculate relevance index from wordcount index
-def calculate_relevance_index(content_directory):
-    # load wordcount index
-    wordcount_index_file = content_directory + "index_wordcount.hdf5"
-    hd5_store = pandas.HDFStore(wordcount_index_file, mode='r')
-    wordcount_index = hd5_store['corpus_index']
-    hd5_store.close()
-
-    # following is a workaround for a pandas bug
-    wordcount_index.index = wordcount_index.index.astype(str)
-
-    # word frequency (per document) from wordcount, aka TF
-    frequency_index = wordcount_index/wordcount_index.sum()
-
-    # penalise short word length
-    for word in frequency_index.index.values:
-        frequency_index.loc[word] = frequency_index.loc[word] * numpy.tanh(len(word)/5.0)
-        pass
-
-    # inverse document frequency
-    for word in frequency_index.index.values:
-        documents = frequency_index.loc[word]
-        matching_documents = documents[documents > 0]
-        inverse_document_frequency = 1.0 - (len(matching_documents) / len(documents))
-        # print("word =", word, " idf = ", inverse_document_frequency)
-        frequency_index.loc[word] = frequency_index.loc[word] * inverse_document_frequency
-        pass
-
-    # save relevance index
-    relevance_index_file = content_directory + "index_relevance.hdf5"
-    print("saving corpus relevance index ... ", relevance_index_file)
-    hd5_store = pandas.HDFStore(relevance_index_file, mode='w')
-    hd5_store['corpus_index'] = frequency_index
-    hd5_store.close()
-    pass
-
-
 # query wordcount index
 def search_wordcount_index(content_directory, search_query):
     # load index if it already exists
@@ -170,44 +118,3 @@ def search_wordcount_index(content_directory, search_query):
     # filter out those with score of zero
     results = results[results > 0]
     return results.sort_values(ascending=False)
-
-
-# query relevance index
-def search_relevance_index(content_directory, search_query):
-    # load index if it already exists
-    relevance_index_file = content_directory + "index_relevance.hdf5"
-    hd5_store = pandas.HDFStore(relevance_index_file, mode='r')
-    relevance_index = hd5_store['corpus_index']
-    hd5_store.close()
-
-    # following is a workaround for a pandas bug
-    relevance_index.index = relevance_index.index.astype(str)
-
-    # query string to list of search terms
-    search_query_list = search_query.split()
-
-    # do query
-    documents = relevance_index.loc[search_query_list]
-    # sum the scores
-    results = documents.sum()
-    # multiply the scores
-    #results = documents.prod()
-    # filter out those with score of zero
-    results = results[results > 0]
-    return results.sort_values(ascending=False)
-
-
-# get words ordered by relevance (across all documents)
-def get_words_by_relevance(content_directory):
-    # load relevance index
-    relevance_index_file = content_directory + "index_relevance.hdf5"
-    hd5_store = pandas.HDFStore(relevance_index_file, mode='r')
-    relevance_index = hd5_store['corpus_index']
-    hd5_store.close()
-
-    # following is a workaround for a pandas bug
-    relevance_index.index = relevance_index.index.astype(str)
-
-    # sum the relevance scores for a word across all documents, sort
-    word_relevance = relevance_index.sum(axis=1).sort_values(ascending=False)
-    return word_relevance
