05992374e21d4a904c5d5e336247a9723321962b
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2017-04-10 15:17:24 +0100

    update to fix changes in libs

--- text_mining_toolkit/visualisation.py
+++ text_mining_toolkit/visualisation.py
@@ -14,16 +14,15 @@ import random
 # word cloud
 def plot_wordcloud(word_count, most_common=None):
     # wordcloud object
-    wc = wordcloud.WordCloud(max_words=100, width=1600, height=800, background_color="white", margin=10,
+    wc = wordcloud.WordCloud(max_words=100, width=1600, height=900, background_color="white", margin=10,
                              prefer_horizontal=1.0)
 
     # words and plot sizes (word count, relevance, etc)
-    words_and_numbers = list(word_count.items())
 
-    wc.generate_from_frequencies(words_and_numbers[:most_common])
+    wc.generate_from_frequencies(dict(word_count.most_common(most_common)))
 
     # plot wordcloud
-    plt.figure(dpi=300, figsize=(16, 8))
+    plt.figure(dpi=600, figsize=(8, 4))
     plt.imshow(wc)
     plt.axis("off")
     pass
