58c387aeaeedfc5e5234cb8ed41e90a5b7fb3051
makeyourowntextminingtoolkit <makeyourowntextminingtoolkit@gmail.com>
2017-04-18 11:36:42 +0100

    dictionary modile with dictionary loader

---
+++ text_mining_toolkit/dictionary.py
@@ -0,0 +1,10 @@
+# module for working with language dictionaries
+
+import pandas
+
+# get dictionary words
+def get_dictionary_words(dictionary):
+    # load dictionary
+    dictionary_file = "dictionaries/" + dictionary
+    words_df = pandas.read_table(dictionary_file)
+    return words_df
